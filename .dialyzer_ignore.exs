ad602400bb4902f17b0b8913be163e65e994a066
Seungjin Kim <skim@proove.com>
2022-11-25 09:01:40 -0800

    UPDATE: tweak ignore text

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,4 @@
 [
-  {":0:unknown_function Function Phoenix.LiveReloader.call/2 does not exist."},
-  {":0:unknown_function Function Phoenix.LiveReloader.init/1 does not exist."}
+  {"Warning: Function Phoenix.LiveReloader.call/2 does not exist."},
+  {"Warning: Function Phoenix.LiveReloader.init/1 does not exist."}
 ]
