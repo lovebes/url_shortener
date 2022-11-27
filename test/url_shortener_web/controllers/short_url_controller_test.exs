e55364c3712ad42627aba5fe85885bdf69d7fdc9
Seungjin Kim <skim@proove.com>
2022-11-26 22:44:54 -0800

    UPDATE: test to also confirm hit count

--- test/url_shortener_web/controllers/short_url_controller_test.exs
+++ test/url_shortener_web/controllers/short_url_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule UrlShortenerWeb.ShortUrlControllerTest do
   use UrlShortenerWeb.ConnCase
+  alias UrlShortener.TinyUrls
   import UrlShortener.TinyUrlsFixtures
 
   defp create_tiny_url(_) do
@@ -11,9 +12,15 @@ defmodule UrlShortenerWeb.ShortUrlControllerTest do
   describe "redirection" do
     setup [:create_tiny_url]
 
-    test "GET /tiny should redirect on valid short url", %{conn: conn} do
+    test "GET /tiny should redirect on valid short url and increment hit count", %{
+      conn: conn,
+      tiny_url: tiny_url
+    } do
       conn = get(conn, ~p"/tiny/short_url")
+      prev_hit_count = tiny_url.hit_count
       assert "http://haha.com" = redirected_to(conn, 302)
+      updated_tiny_url = TinyUrls.get_tiny_url!(tiny_url.id)
+      assert prev_hit_count + 1 == updated_tiny_url.hit_count
     end
 
     test "GET /tiny should show error page on invalid short url", %{conn: conn} do
