ee069e10072e3ec636bd7f3d428795bbd05a65b0
Seungjin Kim <skim@proove.com>
2022-11-28 18:59:35 -0800

    REMOVE: functions not needed for Index

--- lib/url_shortener_web/live/tiny_url_live/index.ex
+++ lib/url_shortener_web/live/tiny_url_live/index.ex
@@ -2,7 +2,6 @@ defmodule UrlShortenerWeb.TinyUrlLive.Index do
   use UrlShortenerWeb, :live_view
   import UrlShortenerWeb.TinyUrlLive.Components
   alias UrlShortener.TinyUrls
-  alias UrlShortener.TinyUrls.TinyUrl
 
   @impl true
   def mount(_params, _session, socket) do
@@ -12,18 +11,6 @@ defmodule UrlShortenerWeb.TinyUrlLive.Index do
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Tiny url")
-    |> assign(:tiny_url, TinyUrls.get_tiny_url!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Tiny url")
-    |> assign(:tiny_url, %TinyUrl{})
   end
 
   defp apply_action(socket, :index, _params) do
